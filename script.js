32c43beecef4b51a1c805c1516cf7e4fde31e64e
NinaFedorchenko <ninapoberevskaya@gmail.com>
2021-12-07 13:40:16 +0200

    Adjustments.statement for and if...else.

--- script.js
+++ script.js
@@ -8,12 +8,27 @@ const personalMovieDB = {
   privat: false,
 };
 
-const answerFirst = prompt("Один из последних просмотренных фильмов?", ""),
-answerSecond = prompt("На сколько оцените его?", ""),
-answerThird = prompt("Один из последних просмотренных фильмов?", ""),
-answerFour = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const answerFirst = prompt("Один из последних просмотренных фильмов?", ""),
+    answerSecond = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[answerFirst] = answerSecond;
-personalMovieDB.movies[answerThird] = answerFour;
+  if (answerFirst && answerSecond && answerFirst.length < 50) {
+    personalMovieDB.movies[answerFirst] = answerSecond;
+    console.log("DONE");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+
+if(personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы классически зритель");
+} else if (personalMovieDB >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
